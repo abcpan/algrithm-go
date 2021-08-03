a6f4af461b9eb2a197ede8eb6f30b5e0cab013b6
pangc01 <pangc01@mingyuanyun.com>
2021-08-03 18:03:54 +0800

    aa

--- algorithm/numSquares/index.go
+++ algorithm/numSquares/index.go
@@ -1,1 +1,32 @@
 package numSquares
+
+
+func numSquares(n int) int {
+	queue := []int{0}
+	visited := map[int]int {0: 0}
+	level := 0
+
+	for len(queue) != 0 {
+		size := len(queue)
+		level++
+		// 循环一层
+		for i := 0; i < size;i++ {
+			digit := queue[0]
+			queue = queue[1:]
+			for j := 1; j <= n; j++ {
+				value := digit + j * j
+				if value == n {
+					return level
+				}
+				if value > n {
+					break
+				}
+				if _, isIn := visited[value]; !isIn {
+					queue = append(queue, value)
+					visited[value] = 0
+				}
+			}
+		}
+	}
+	return level
+}
--- algorithm/numSquares/index_test.go
+++ algorithm/numSquares/index_test.go
@@ -1,1 +1,23 @@
 package numSquares
+
+import (
+	"testing"
+)
+
+type NumSquareTest struct {
+	Input int
+	Want int
+}
+
+func TestNumsSquare(t *testing.T) {
+	tests := []NumSquareTest {
+		{12, 3},
+		{13, 2},
+	}
+	for _, tt := range tests {
+		ret := numSquares(tt.Input)
+		if ret != tt.Want {
+			t.Errorf("expected %v, got %v", tt.Want, ret)
+		}
+	}
+}
